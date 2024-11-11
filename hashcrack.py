95888e81831170cba06d678f8d00e9118a157b28
ente0 <156426041+ente0v1@users.noreply.github.com>
2024-11-11 20:03:26 +0100

    Update

--- hashcrack.py
+++ hashcrack.py
@@ -2,7 +2,7 @@ import os
 import time
 import sys
 from functions import (
-    define_default_parameters, define_windows_parameters, clear_screen, show_title, show_menu, handle_option)
+    define_default_parameters, define_windows_parameters, clear_screen, show_menu, handle_option)
 
 define_windows_parameters()
 define_default_parameters()
@@ -11,7 +11,6 @@ default_os = "Windows"
 
 while True:
     clear_screen()
-    show_title()
     show_menu(default_os)
     
     user_option = input("Enter option (1-4, or Q to quit): ").strip()
